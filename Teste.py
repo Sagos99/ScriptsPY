93c12c50551271e6e9e0e1264bb2788d2ae5a90b
Samuel <samuel.ramos@waybee.com.br>
2019-04-04 12:00:52 -0300

    Arquivo de teste

--- Teste.py
+++ Teste.py
@@ -1,17 +1,2 @@
 # -*- coding: utf-8 -*-
 
-num = 82
-
-for x in range(7):
-    print(num)
-    for x in range(2):
-        num += 28
-        print(num)
-
-        num -= 13
-        print(num)
-
-        num += 28
-        num *= 2
-        print(num)
-        num -= 13
