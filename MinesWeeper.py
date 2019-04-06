f7474010e7ecc53200a5de7a389261d0b835cc19
Sagos <sagoswins@gmail.com>
2019-04-06 18:48:53 -0300

    Falhou tudo

--- MinesWeeper.py
+++ MinesWeeper.py
@@ -1,15 +1,26 @@
 ##########################################
 ########### Bot campo minado #############
-############# Samuel Ramos ###############
-######## Criado dia: 05/11/2018 ##########
+########### By: Samuel Ramos #############
 ##########################################
 
 
-
 import pyautogui
+from time import sleep
+from random import randint
+# from playsound import playsound
 
-print(pyautogui.position())
 
-pyautogui.click(1501, 972)
+sleep(2)
 
-pyautogui.typewrite('Teste!\n', interval=0.1)
+# img = pyautogui.screenshot(region=(151, 99, 1059, 566)) # Captura o campo minado
+
+pyautogui.click(randint(151,1059),randint(99,566)) # Pega um valor aleatório para fazer o first_click
+pyautogui.moveTo(1,1)
+
+
+numbers_one = list(pyautogui.locateAllOnScreen(r'img\01.png'))
+print('Encontrou %s números' % str(len(numbers_one)))
+
+
+img = pyautogui.screenshot(region=(151, 99, 1059, 566)) # Captura o campo minado
+img.save(r'img\tela.png')
